2b37a7bd1e6b88be74a5d1b6bed6bcff3ba35726
nilsaxling <nilsaxling@gmail.com>
2024-01-16 13:43:50 +0100

    DTOs

---
+++ Motorcenter.API.DTO/DTOs/TypeDTOs.cs
@@ -0,0 +1,21 @@
+﻿namespace Motorcenter.API.DTO;
+
+public class TypePostDTO
+{
+    public string Name { get; set; } = string.Empty;
+}
+
+public class TypePutDTO : TypePostDTO
+{
+    public int Id { get; set; }
+}
+
+public class TypeGetDTO : TypePutDTO
+{
+    //public List<FilterGetDTO>? Filters { get; set; }
+}
+
+public class CategorySmallGetDTO : TypePutDTO
+{
+
+}
