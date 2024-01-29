aad320c3df2dc081213b5c4fda73cdfcbf1675ff
Emdre001 <emil.dressel@gmail.com>
2024-01-29 13:58:29 +0100

    TypeDbService added

---
+++ Motorcenter.Data/Services/TypeDbService.cs
@@ -0,0 +1,13 @@
+﻿namespace Motorcenter.Data.Services;
+
+public class TypeDbService(MotorcenterContext db, IMapper mapper) : DbService(db, mapper)
+{
+    public override async Task<List<TDto>> GetAsync<TEntity, TDto>()
+    {
+        //IncludeNavigationsFor<Filter>();
+        //IncludeNavigationFor<Vehicle>();
+        var result = await base.GetAsync<TEntity, TDto>();
+        return result;
+    }
+
+}
