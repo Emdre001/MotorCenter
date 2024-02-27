bf123d66fa1818c223dbaca4a9b0effbf4acb83d
nilsaxling <nilsaxling@gmail.com>
2024-02-27 12:28:38 +0100

    Fixat CategoryHttpClient

--- Motorcenter.UI.Http/Clients/CategoryHttpClient.cs
+++ Motorcenter.UI.Http/Clients/CategoryHttpClient.cs
@@ -1,39 +1,27 @@
-﻿namespace Motorcenter.UI.Http.Client;
-global using System.Text.Json;
-global using System.Net.Http;
+﻿
 
-namespace Motorcenter.UI.Http.Clients;
+namespace eShop.UI.Http.Clients;
 
 public class CategoryHttpClient
 {
+    private readonly HttpClient _httpClient;
+    string _baseAddress = "https://localhost:7054/api/";
 
-    public HttpClient Client { get; }
-    string BaseAddress = "https://localhost:7054/api/";
-
-
-    public CategoryHttpClient(HttpClient _httpClient)
+    public CategoryHttpClient(HttpClient httpClient)
     {
-        Client = _httpClient;
-        Client.BaseAddress = new Uri($"{BaseAddress} categorys");
-
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri($"{_baseAddress}categorys");
     }
 
     public async Task<List<TypeGetDTO>> GetCategoriesAsync()
     {
         try
         {
-            using HttpResponseMessage response = await Client.GetAsync("");
-            response.EnsureSuccessStatusCode();
-  /*  public async Task<List<TypeGetDTO>> GetCategoriesAsync()
-    {
-        try
-        {
-            using HttpResponseMessage response = await Client.GetAsync("");
+            using HttpResponseMessage response = await _httpClient.GetAsync("");
             response.EnsureSuccessStatusCode();
 
             var result = JsonSerializer.Deserialize<List<TypeGetDTO>>(await response.Content.ReadAsStreamAsync(),
                 new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-
 
             return result ?? [];
         }
--- Motorcenter.UI.Http/Usings.cs
+++ Motorcenter.UI.Http/Usings.cs
@@ -1,2 +1,3 @@
 ﻿global using System.Text.Json;
 global using Motorcenter.API.DTO;
+global using System.Net.Http;
